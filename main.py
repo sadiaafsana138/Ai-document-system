f5aa27d793a0ad291386bd909d29747851c81f7e
sadiaafsana138 <s.sadiaafsanamumui@gmail.com>
2026-05-14 23:17:19 +0600

    Embedding

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from app.ocr import extract_text_from_pdf
+from app.chunking import chunk_text
+from app.embeddings import get_embeddings
 
 text = extract_text_from_pdf("data/raw/sample.pdf")
 
-print(text)
+chunks = chunk_text(text)
+
+vectors = get_embeddings(chunks)
+
+print("Chunks:", len(chunks))
+print("Vectors shape:", len(vectors), len(vectors[0]))
